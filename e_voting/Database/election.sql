14a1e597bf424559f857123cd7c2cc746583f0c3
Chandrashekhar B <115780527+chandrashekharb369@users.noreply.github.com>
2024-02-28 08:41:01 +0530

    Update election.sql

--- e_voting/Database/election.sql
+++ e_voting/Database/election.sql
@@ -13,7 +13,7 @@ SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
@@ -37,8 +37,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`username`, `password`) VALUES
-('admin', 123),
-('abcd', 1234);
+('chandra' 963),
+('admin', 123);
 
 -- --------------------------------------------------------
 
